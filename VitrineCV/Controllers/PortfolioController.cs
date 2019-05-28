fb107ae48409266e1e7c0be1fd718f675b259199
Nicolas Eisenberg <eisenberg_n@hotmail.fr>
2019-05-29 00:26:53 +0200

    Added the Others portfolio entry

--- VitrineCV/Controllers/PortfolioController.cs
+++ VitrineCV/Controllers/PortfolioController.cs
@@ -30,6 +30,11 @@ namespace VitrineCV.Controllers
             return View();
         }
 
+        public IActionResult Others()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
